0b554f096a38d88fadb4ea70ab3755454250ae81
Douglas Mashonganyika <douglasmasho@gmail.com>
2024-10-03 14:51:49 +0200

    added stuff

---
+++ src/components/forms/EditPatientForm.jsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import "react-datepicker/dist/react-datepicker.css";
+import { db } from "../../firebase/firebase";
+import { doc, setDoc, arrayUnion, updateDoc } from "firebase/firestore";
+import Lottie from "lottie-react";
+import animation from "../../assets/animation/loadingcubes.json";
+import { nanoid } from 'nanoid';
+import toast from "react-hot-toast";
+import { useAuth } from "../../contexts/authContext";
+
+const EditPatientForm = ({ closeModal, getPatient, isDarkMode, fullNameProp, genderProp, dobProp, pid }) => {
+  const [fullName, setFullName] = useState(fullNameProp);
+  const [gender, setGender] = useState(genderProp);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isAdding, setIsAdding] = useState(false);
+  const [startDate, setStartDate] = useState(new Date(dobProp));
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { currentUser } = useAuth();
+
+  const getAge = (birthDate) => {
+    const birthMoment = moment(birthDate.getTime());
+    const currentMoment = moment();
+    const age = currentMoment.diff(birthMoment, "years");
+    return age;
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (!isAdding) {
+      setIsAdding(true);
+      setIsLoading(true);
+      const data = {
+        name,
+        dob: startDate.getTime(),
+        fullName
+      }
+      try {
+        await updateDoc(doc(db, "patients", pid), data);
+      } catch (e) {
+        toast.error(e.code);
+        console.log(e);
+      } finally {
+        setIsAdding(false);
+        setIsLoading(false);
+        getPatient();
+        closeModal();
+      }
+    }
+  };
+
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="u-center-hrz">
+          <Lottie animationData={animation} style={{ width: "500px" }} />
+        </div>
+      ) : (
+        <form
+          onSubmit={onSubmit}
+          className={`space-y-4 ${isDarkMode ? 'text-white' : 'text-gray-900'} u-margin-top-small`}
+          style={{ width: "90%" }}
+        >
+          <div>
+            <label className={`text-sm font-bold ${isDarkMode ? 'text-gray-300' : 'text-gray-700'}`}>Full Name</label>
+            <input
+              type="text"
+              autoComplete="full-name"
+              required
+              value={fullName}
+              onChange={(e) => {
+                setFullName(e.target.value);
+              }}
+              className={`w-full mt-2 px-3 py-2 ${isDarkMode ? 'bg-gray-700 text-white placeholder-gray-400' : 'bg-gray-100 text-gray-900 placeholder-gray-600'} outline-none border ${isDarkMode ? 'border-gray-600' : 'border-gray-300'} focus:border-indigo-600 shadow-sm rounded-lg transition duration-300`}
+            />
+          </div>
+
+          <div>
+            <label className={`text-sm font-bold ${isDarkMode ? 'text-gray-300' : 'text-gray-700'}`}>Gender</label>
+            <select
+              required
+              value={gender}
+              onChange={(e) => {
+                setGender(e.target.value);
+              }}
+              className={`w-full mt-2 px-3 py-2 ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-100 text-gray-900'} outline-none border ${isDarkMode ? 'border-gray-600' : 'border-gray-300'} focus:border-indigo-600 shadow-sm rounded-lg transition duration-300`}
+            >
+              <option value="" disabled>Select an option</option>
+              <option value="male" className={`${isDarkMode ? 'white-text' : 'text-black'}`}>Male</option>
+              <option value="female" className={`${isDarkMode ? 'white-text' : 'text-black'}`}>Female</option>
+            </select>
+          </div>
+
+          <div>
+            <label className={`text-sm font-bold ${isDarkMode ? 'text-gray-300' : 'text-gray-700'}`}>Date of Birth</label>
+            <br />
+            <DatePicker
+              dateFormat="dd/MM/yyyy"
+              placeholderText="dd/mm/yyyy"
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              className={`w-full mt-2 px-3 py-2 ${isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-100 text-gray-900'} outline-none border ${isDarkMode ? 'border-gray-600' : 'border-gray-300'} focus:border-indigo-600 shadow-sm rounded-lg transition duration-300`}
+            />
+          </div>
+
+          {errorMessage && (
+            <span className="text-red-600 font-bold">{errorMessage}</span>
+          )}
+
+          <button
+            type="submit"
+            className={`px-4 py-2 font-medium rounded-lg ${isDarkMode ? 'bg-indigo-600 hover:bg-indigo-700' : 'bg-indigo-700 hover:bg-indigo-800'} text-white transition duration-300`}
+          >
+            Edit Patient
+          </button>
+        </form>
+      )}
+    </>
+  );
+};
+
+export default EditPatientForm;
--- src/pages/PatientPage.jsx
+++ src/pages/PatientPage.jsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react";
-import { Search, BrainCircuit, View, BadgeHelp, User } from "lucide-react";
-import { motion } from "framer-motion";
+import { User, CircleX, Pencil, Trash2 } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 import Header from "../components/common/Header";
 import { useAuth } from "../contexts/authContext/index";
 import { Navigate } from "react-router-dom";
 import { useDarkMode } from "../contexts/darkModeContext";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase/firebase";
-import { getDoc, doc } from "firebase/firestore";
+import { getDoc, doc, deleteDoc } from "firebase/firestore";
 import toast from "react-hot-toast";
 import Lottie from "lottie-react";
 import animation from "../assets/animation/loadingcubes.json";
 import moment from "moment";
 import DiagnosisTable from "../components/users/DiagnosisTableDetect";
 import DiagnosisTable2 from "../components/users/DiagnosisTableAnalysis";
+import { Modal } from 'react-responsive-modal';
+import EditPatientForm from "../components/forms/EditPatientForm";
 
 
 const PatientPage = () => {
@@ -25,7 +27,11 @@ const PatientPage = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [detectDiagnoses, setDetectDiagnoses] = useState([]);
     const [analysisDiagnoses, setAnalysisDiagnoses] = useState([]);
+    const [open, setOpen] = useState(false);
 
+
+    const onOpenModal = () => setOpen(true);
+    const onCloseModal = () => setOpen(false);
 
     useEffect(() => {
         console.log(pid);
@@ -66,12 +72,33 @@ const PatientPage = () => {
         }
     }
 
+    const deleteDiagnosis = async(pid, did, grade)=>{
+        try{
+            //remove the document from the rightful collection.
+            // check if diagnosis has grade
+            setIsLoading(true);
+            let collectionString = grade ? "analysisDiagnosis" : "detectDiagnosis";
+            await deleteDoc(doc(db, collectionString, did))
+
+            //remove it from the diagnosisSet of the patient
+            await updateDoc(doc())
+        }catch(e){
+            toast.error(e.code);
+            console.log(e)
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
+    const deletePatient = ()=>{
+
+    }
+
     // Set dynamic colors based on the mode
     const backgroundColor = isDarkMode ? "bg-gray-800" : "bg-white";
     const borderColor = isDarkMode ? "border-gray-700" : "border-gray-300";
     const textColor = isDarkMode ? "text-gray-100" : "text-gray-900";
     const secondaryTextColor = isDarkMode ? "text-gray-400" : "text-gray-600";
-
     return (
         <div className={`flex-1 overflow-auto relative z-10 ${backgroundColor}`}>
             {!userLoggedIn ? (
@@ -97,12 +124,34 @@ const PatientPage = () => {
                             >
                                 <h2 className={`bigger-text u-margin-bottom-small ${textColor}`}>Details</h2>
                                 <p className={textColor}>Name: {patient.fullName}</p>
-                                <p className={textColor}>Gender: {patient.gender}</p>
+                                <p className={`${textColor} capitalize-text`}>Gender: {patient.gender}</p>
                                 <p className={textColor}>Age: {getAge(patient.dob)}</p>
-                                
+
                                 <p className={textColor}>Last Diagnosis: {
                                     patient.lastDiagnosis ?
-                                moment(patient.lastDiagnosis).format("Do MMMM YYYY, h:mm:ss a") : "Not yet diagnosed"}</p>
+                                        moment(patient.lastDiagnosis).format("Do MMMM YYYY, h:mm:ss a") : "Not yet diagnosed"}</p>
+                                <div className="u-margin-top-small" style={{display: "flex"}}>
+                                    <motion.button
+                                        className="bg-blue-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto mr-5"
+                                        style={{ display: "flex", alignItems: "center" }}
+                                        whileHover={{ scale: 1.05 }}
+                                        whileTap={{ scale: 0.9 }}
+                                        onClick={onOpenModal}
+                                    >
+                                        <Pencil className="mr-2" />
+                                        Edit Patient
+                                    </motion.button>
+
+                                    <motion.button
+                                        className="bg-red-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto mr-5"
+                                        style={{ display: "flex", alignItems: "center" }}
+                                        whileHover={{ scale: 1.05 }}
+                                        whileTap={{ scale: 0.9 }}
+                                    >
+                                        <Trash2 className="mr-2" />
+                                        Delete Patient
+                                    </motion.button>
+                                </div>
 
                             </motion.div>
                         </main>
@@ -121,19 +170,43 @@ const PatientPage = () => {
                         }
 
                         {
-                            analysisDiagnoses.length !== 0?
-                            <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
-                            <motion.div
-                                className={`shadow-lg rounded-xl p-6 border ${backgroundColor} ${borderColor}`}
-                                initial={{ opacity: 0, y: 20 }}
-                                animate={{ opacity: 1, y: 0 }}
-                                transition={{ delay: 0.5, duration: 1 }}
-                            >
-                                <DiagnosisTable2 diagnoses={analysisDiagnoses} />
-                            </motion.div>
-                        </main> : null
+                            analysisDiagnoses.length !== 0 ?
+                                <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+                                    <motion.div
+                                        className={`shadow-lg rounded-xl p-6 border ${backgroundColor} ${borderColor}`}
+                                        initial={{ opacity: 0, y: 20 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        transition={{ delay: 0.5, duration: 1 }}
+                                    >
+                                        <DiagnosisTable2 diagnoses={analysisDiagnoses} />
+                                    </motion.div>
+                                </main> : null
                         }
-
+          <Modal
+            open={open}
+            onClose={onCloseModal}
+            center
+            classNames={{
+              modal: `${
+                isDarkMode
+                  ? "bg-gray-800 text-white"
+                  : "bg-white text-gray-900"
+              } backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700`,
+            }}
+            styles={{
+              modal: {
+                width: "80%",
+              },
+            }}
+            closeIcon={<CircleX 
+				style={isDarkMode ? { color: "white" } : {color:" black"}}
+				 />}
+          >
+            <h2 className="text-2xl font-semibold">
+              Edit Patient
+            </h2>
+            <EditPatientForm closeModal={onCloseModal} getPatient={getPatient} isDarkMode={isDarkMode} pid={pid} fullNameProp={patient.fullName} genderProp={patient.gender} dobProp={patient.dob}/>
+          </Modal>
                     </>
             )}
         </div>
